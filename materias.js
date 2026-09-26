afc722c3ae60495039852d61b1cb0d0788919414
Matematicas UNA Yaracuy <ivanglopezp@yahoo.com>
2026-09-25 21:53:54 -0400

    Update CSV file paths for subjects

--- materias.js
+++ materias.js
@@ -11,15 +11,15 @@ const MATERIAS_CONFIG = {
     codigo: "178",
     nombre: "Matemática II",
     scriptUrl: "https://script.google.com/macros/s/AKfycbyVVmX4dJ1eBNpn2ezcjLDu23R8smSh5Z1-nFgUUVA3OBKfMAqUUk3FT0J8LZT4BKF03w/exec",
-    csvPreguntas: "./data/178_preguntas.csv",
-    csvEjercicios: "./data/178_ejercicios.csv"
-  }
+    csvPreguntas: "./preguntas.csv",
+    csvEjercicios: "./ejercicios.csv"
+  },    
   "768": {
     codigo: "768",
     nombre: "Topologia",
     scriptUrl: "https://script.google.com/macros/s/AKfycbyhcJcZlg-JWzBwl5Y627_5H2nfXQQoZT6iw8oCLL4PmNABLmzx0wrTLhNoMdSdz00k6A/exec",
-    csvPreguntas: "./data/178_preguntas.csv",
-    csvEjercicios: "./data/178_ejercicios.csv"
+    csvPreguntas: "./preguntas.csv",
+    csvEjercicios: "./ejercicios.csv"
   }
 };
 
