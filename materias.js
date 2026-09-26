3e1c80238f96979f7dd656c79990d42cc76ca27d
Matematicas UNA Yaracuy <ivanglopezp@yahoo.com>
2026-09-25 22:11:29 -0400

    Update materias.js

--- materias.js
+++ materias.js
@@ -1,29 +1,30 @@
-// materias.js - Catálogo de Materias y Backend Apps Script
+// materias.js - Catálogo de Materias centralizado
+const WEB_APP_URL_CENTRAL = "https://script.google.com/macros/s/AKfycby74QRdNkKjtV_I_auAImfvpi2BJMrzUbN6RdwuIDpPnDzua9WaXCd-xVisA45Z5252/exec";
+
 const MATERIAS_CONFIG = {
   "175": {
     codigo: "175",
     nombre: "Matemática I",
-    scriptUrl: "https://script.google.com/macros/s/AKfycby74QRdNkKjtV_I_auAImfvpi2BJMrzUbN6RdwuIDpPnDzua9WaXCd-xVisA45Z5252/exec",
+    scriptUrl: WEB_APP_URL_CENTRAL,
     csvPreguntas: "./preguntas.csv",
     csvEjercicios: "./ejercicios.csv"
   },
   "178": {
     codigo: "178",
     nombre: "Matemática II",
-    scriptUrl: "https://script.google.com/macros/s/AKfycbyVVmX4dJ1eBNpn2ezcjLDu23R8smSh5Z1-nFgUUVA3OBKfMAqUUk3FT0J8LZT4BKF03w/exec",
+    scriptUrl: WEB_APP_URL_CENTRAL,
     csvPreguntas: "./preguntas.csv",
     csvEjercicios: "./ejercicios.csv"
   },    
   "768": {
     codigo: "768",
     nombre: "Topologia",
-    scriptUrl: "https://script.google.com/macros/s/AKfycbyhcJcZlg-JWzBwl5Y627_5H2nfXQQoZT6iw8oCLL4PmNABLmzx0wrTLhNoMdSdz00k6A/exec",
+    scriptUrl: WEB_APP_URL_CENTRAL,
     csvPreguntas: "./preguntas.csv",
     csvEjercicios: "./ejercicios.csv"
   }
 };
 
-// Materia activa guardada en localStorage o por defecto '175'
 let materiaActiva = localStorage.getItem('mateuna_materia_activa') || '175';
 
 function getMateriaActual() {
